500ba831326464b13c19e27a7add03cc61e1f30d
CarlosPulgar <carlos911.cp@gmail.com>
2025-12-05 10:35:05 -0300

    el navbar, sideMenu y navegacion listo prototipo ecom-react.js 16

---
+++ components/Header.tsx
@@ -0,0 +1,36 @@
+import CArtIcon from "./CArtIcon";
+import Container from "./Container";
+import FavoriteButton from "./FavoriteButton";
+import Logo from "./Logo";
+import MenuHeader from "./MenuHeader";
+import MobileMenu  from "./MobileMenu";
+import SearchBar from "./SearchBar";
+import SignIn from "./SignIn";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-50 py-5 bg-white/70 backdrop-blur-md">
+        <Container className="flex items-center justify-between text-lightColor">
+          {/* logo */}
+          <div  className="w-auto md:w-1/3 flex items-center gap-2.5 justify-start md:gap-0">
+            <MobileMenu></MobileMenu>
+            <Logo></Logo>
+          </div>
+
+          {/* navButton */}
+          <MenuHeader ></MenuHeader>
+
+          {/*  NavAdmin*/}
+          <div className="w-auto md:w-1/3 flex items-center justify-end gap-5">
+            <SearchBar></SearchBar>
+            <CArtIcon></CArtIcon>
+            <FavoriteButton></FavoriteButton>
+            <SignIn></SignIn>
+          </div>
+        </Container>
+     
+    </header>
+  );
+};
+
+export default Header;
