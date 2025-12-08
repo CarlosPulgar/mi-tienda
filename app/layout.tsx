94d7c2541e8a28b8f037d8ad512df8e891680944
CarlosPulgar <carlos911.cp@gmail.com>
2025-12-08 14:55:19 -0300

    feat: restructure layout and remove unused components; add category schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,11 @@
-import { Metadata } from "next";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import { ClerkProvider } from "@clerk/nextjs";
-
-export const metadata: Metadata = {
-  title: {
-    template: "%s - Shopcart online store",
-    default: "shopcart online sytore",
-  },
-  description: "Shopcart online store, your one stop for all your needs",
-};
-
+/* import { Toaster } from "react-hot-toast"; */
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className="font-poppins antialiased">
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-1">{children}</main>
-            {/* <Toaster
+    <html lang="en">
+      <body className="font-poppins antialiased">
+        {children}
+        {/* <Toaster
           position="bottom-right"
           toastOptions={{
             style: {
@@ -29,11 +14,8 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
             },
           }}
         /> */}
-            <Footer />
-          </div>
-        </body>
-      </html>
-    </ClerkProvider>
+      </body>
+    </html>
   );
 };
 export default RootLayout;
