ea4ea5144b04e0436293928244b25517c650bc43
CarlosPulgar <carlos911.cp@gmail.com>
2025-12-09 11:42:04 -0300

    Refactor code structure for improved readability and maintainability

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,11 +1,13 @@
 import Container from "@/components/Container";
 import HomeBanner from "@/components/HomeBanner";
+import ProductGrid from "@/components/ProductGrid";
 import { Button } from "@/components/ui/button";
 
 const Home = () => {
   return (
     <Container className=" bg-shop-light-pink ">
-     <HomeBanner/>
+      <HomeBanner />
+      <ProductGrid />
     </Container>
   );
 };
