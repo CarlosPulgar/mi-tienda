605fedde3b5b0cf2f85374293f6caec99c6cfb74
CarlosPulgar <carlos911.cp@gmail.com>
2025-12-09 16:14:41 -0300

    feat: update color variables in globals.css and enhance ProductCard component with new styles; add ProductSideMenu component

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -5,15 +5,18 @@ import React from "react";
 import Link from "next/link";
 import { StarIcon } from "@sanity/icons";
 import { Flame } from "lucide-react";
-/* import PriceView from "./PriceView";
-import Title from "./Title";
+import PriceView from "./PriceView";
+
 import ProductSideMenu from "./ProductSideMenu";
-import AddToCartButton from "./AddToCartButton"; */
+import AddToCartButton from "./AddToCartButton";
+import { Title } from "./ui/text";
 
 const ProductCard = ({ product }: { product: Product }) => {
+  console.log(product);
+
   return (
     <div className="text-sm border-[1px] rounded-md border-darkBlue/20 group bg-white">
-      {/* <div className="relative group overflow-hidden bg-shop_light_bg">
+      <div className="relative group overflow-hidden bg-shop-light-bg">
         {product?.images && (
           <Link href={`/product/${product?.slug?.current}`}>
             <Image
@@ -22,43 +25,45 @@ const ProductCard = ({ product }: { product: Product }) => {
               width={500}
               height={500}
               priority
-              className={`w-full h-64 object-contain overflow-hidden transition-transform bg-shop_light_bg duration-500 
+              className={`w-full h-64 object-contain overflow-hidden transition-transform bg-shop-light-bg duration-500 
               ${product?.stock !== 0 ? "group-hover:scale-105" : "opacity-50"}`}
             />
           </Link>
         )}
-        <ProductSideMenu product={product} />
+        {<ProductSideMenu product={product} />}
         {product?.status === "sale" ? (
-          <p className="absolute top-2 left-2 z-10 text-xs border border-darkColor/50 px-2 rounded-full group-hover:border-lightGreen hover:text-shop_dark_green hoverEffect">
+          <p className="absolute top-2 left-2 z-10 text-xs border border-darkColor/50 px-2 rounded-full group-hover:border-lightGreen hover:text-shop-dark-green hoverEffect">
             Sale!
           </p>
         ) : (
           <Link
             href={"/deal"}
-            className="absolute top-2 left-2 z-10 border border-shop_orange/50 p-1 rounded-full group-hover:border-shop_orange hover:text-shop_dark_green hoverEffect"
+            className="absolute top-2 left-2 z-10 border border-shop_orange/50 p-1 rounded-full group-hover:border-shop_orange hover:text-shop-dark-green hoverEffect"
           >
             <Flame
               size={18}
               fill="#fb6c08"
-              className="text-shop_orange/50 group-hover:text-shop_orange hoverEffect"
+              className="text-shop-orange/50 group-hover:text-shop-orange hoverEffect"
             />
           </Link>
         )}
       </div>
       <div className="p-3 flex flex-col gap-2">
         {product?.categories && (
-          <p className="uppercase line-clamp-1 text-xs font-medium text-lightText">
+          <p className="uppercase line-clamp-1 text-xs font-medium text-shop-light-text">
             {product.categories.map((cat) => cat).join(", ")}
           </p>
         )}
-        <Title className="text-sm line-clamp-1">{product?.name}</Title>
+
+       { <Title className="text-sm line-clamp-1">{product?.name}</Title>}
+
         <div className="flex items-center gap-2">
           <div className="flex items-center">
             {[...Array(5)].map((_, index) => (
               <StarIcon
                 key={index}
                 className={
-                  index < 4 ? "text-shop_light_green" : " text-lightText"
+                  index < 4 ? "text-shop-light-green/30" : " text-shop-light-text"
                 }
                 fill={index < 4 ? "#93D991" : "#ababab"}
               />
@@ -70,20 +75,23 @@ const ProductCard = ({ product }: { product: Product }) => {
         <div className="flex items-center gap-2.5">
           <p className="font-medium">In Stock</p>
           <p
-            className={`${product?.stock === 0 ? "text-red-600" : "text-shop_dark_green/80 font-semibold"}`}
+            className={`${
+              product?.stock === 0
+                ? "text-red-600"
+                : "text-shop-dark-green/80 font-semibold"
+            }`}
           >
             {(product?.stock as number) > 0 ? product?.stock : "unavailable"}
           </p>
         </div>
 
-        <PriceView
+        {/*  <PriceView
           price={product?.price}
           discount={product?.discount}
           className="text-sm"
         />
-        <AddToCartButton product={product} className="w-36 rounded-full" />
-      </div> */}
-      <h1>product card</h1>
+        <AddToCartButton product={product} className="w-36 rounded-full" /> */}
+      </div>
     </div>
   );
 };
